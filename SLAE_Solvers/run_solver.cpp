71baefdd5278e8d73b048df5e1aef91a46a7efe1
lonertutu <wildfire.aei@gmail.com>
2023-02-09 21:16:46 +0300

    solution

--- SLAE_Solvers/run_solver.cpp
+++ SLAE_Solvers/run_solver.cpp
@@ -1,22 +1,34 @@
 #include "run_solver.hpp"
 #include <iostream>
 
-namespace SLAE::Solver{
-std::vector<double> solver_method_run(Tri_matrix::Tridiagonal_matrix &matrix, const std::vector<double> &free_coef) {
-    
-    std::vector<double> solution(matrix.size());
+namespace SLAE::Solver
+{
+    std::vector<double> solver_method_run(Tri_matrix::Tridiagonal_matrix &matrix, const std::vector<double> &free_coef)
+    {
 
-    std::vector<double> alpha_beta(matrix.size()*2);
+        std::vector<double> solution(matrix.size());
 
+        std::vector<double> alpha_beta(matrix.size() * 2);
 
-    alpha_beta[0] = - matrix(0, 1) / matrix(0, 0);
-    alpha_beta[1] = free_coef[0] / matrix(0, 0);
+        alpha_beta[0] = -matrix(0, 1) / matrix(0, 0);
+        alpha_beta[1] = free_coef[0] / matrix(0, 0);
 
-for (int i = 1; i < matrix.size() - 1; ++i) {
-        alpha_beta[2*i] = - matrix(i, 2) / (matrix(i, 0)*free_coef[2*(i-1)] + matrix(i, 1));
-        alpha_beta[2 * i + 1] = (free_coef[i] - matrix(i, 0) * alpha_beta[2 * (i - 1) + 1]) /(matrix(i, 0) * alpha_beta[2 * (i - 1)] + matrix(i, 1));
+        for (int i = 1; i < matrix.size() - 1; ++i)
+        {
+            alpha_beta[2 * i] = -matrix(i, 2) / (matrix(i, 0) * free_coef[2 * (i - 1)] + matrix(i, 1));
+
+            alpha_beta[2 * i + 1] = (free_coef[i] - matrix(i, 0) * alpha_beta[1 + 2 * (i - 1)]) / 
+            (matrix(i, 0) * alpha_beta[2 * (i - 1)] + matrix(i, 1));
+        }
+
+        // обратная прогонка
+
+        for (int j = 1; j < matrix.size() - 1; ++j)
+        {
+            solution[(matrix.size() - 1)] = (free_coef[matrix.size() - 1] - matrix((matrix.size() - 1), 1) * alpha_beta[(matrix.size() - 2)]) / 
+            (matrix((matrix.size() - 1), 2) + matrix((matrix.size() - 1), 1) * alpha_beta[(matrix.size() - 2)]);
+        }
+
+        return solution;
     }
-
-
-}
 }
