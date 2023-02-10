148f43302e5400e3da22babf46a6b2eee6fee2d3
lonertutu <wildfire.aei@gmail.com>
2023-02-10 09:41:22 +0300

    reverse running

--- Matrixes/tridiagonal_matrix.hpp
+++ Matrixes/tridiagonal_matrix.hpp
@@ -2,6 +2,14 @@
 
 namespace SLAE::Tri_matrix {
 
+/*!
+    \brief Parent class of Tridiagonal Matrix
+
+    \param size_matrix Matrix size
+    \param matrix The values ​​of the coefficients in the matrix
+    
+    \warning The matrix is ​​square.
+*/
 class Tridiagonal_matrix {
 
     private:
@@ -9,7 +17,13 @@ class Tridiagonal_matrix {
         std::vector<double> matrix; //matrix coeff
 
     public:
-        inline explicit Tridiagonal_matrix(int size) : size_matrix(size), matrix(std::vector<double>(3 * size)) {}; //явно определяем конструктор
+        /*!
+        \brief Constructor of Tridiagonal Matrix 
+
+        \param[in] size Number of rows or columns
+
+        */
+        inline explicit Tridiagonal_matrix(int size) : size_matrix(size), matrix(std::vector<double>(3 * size)) {};
 
         static Tridiagonal_matrix zero_field(int size); //один раз в программе выделяем место для полей с ноликами.
          
