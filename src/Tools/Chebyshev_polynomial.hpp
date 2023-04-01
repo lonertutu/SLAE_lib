223253b6c891e41d42670b93f87f6f5e676dea64
lonertutu <wildfire.aei@gmail.com>
2023-04-01 17:08:01 +0300

    Ch

--- src/Tools/Chebyshev_polynomial.hpp
+++ src/Tools/Chebyshev_polynomial.hpp
@@ -15,13 +15,11 @@
 
 */
 template <typename T>
-std::vector<T> Solutions(const uint32_t r, const T lambda_min, const T lambda_max) {
-
-    uint32_t n = std::pow(2, r);
+std::vector<T> chebRoots(const uint32_t n, const T lambda_min, const T lambda_max) {
     std::vector<T> roots(n);
 
     const T cos_const = std::cos(M_PI/n);
-    const T sin_const = std::cos(M_PI/n);
+    const T sin_const = std::sin(M_PI/n);  // Ошибка
     double sinB = std::sin(M_PI/(2*n));
     roots[0] = std::cos(M_PI/(2*n));
 
@@ -34,5 +32,28 @@ std::vector<T> Solutions(const uint32_t r, const T lambda_min, const T lambda_ma
         roots[k - 1] = 0.5*((lambda_max + lambda_min) + roots[k-1]*(lambda_max - lambda_min));
     }
     return roots;
-    // написать перемешалку
+}
+
+/*!
+    \brief Function of shuffling root indices for better convergence with usage of recursion
+
+    \param r - power of two
+
+*/
+std::vector<unsigned int> chebRootsShake(unsigned int r) {
+    uint32_t n = std::pow(r, 2);  // не использовать pow
+    //std::vector<uint32_t> indexes(n);
+    std::vector<uint32_t> next(n);
+    std::vector<uint32_t> indexes = {0,1};
+    uint32_t order = 2;
+    for (uint32_t i = 1; i < r; ++i) {
+        order = std::pow(2, i + 1);
+        for (uint32_t j = 0; j < order - 1; j += 2){
+            next[j] = indexes[j/2];
+            next[j + 1] = order - 1 - next[j];
+        }
+        indexes = next;
+    }
+    return indexes;
+
 }
--- tests/Chebyshev.cpp
+++ tests/Chebyshev.cpp
@@ -6,6 +6,19 @@
 
 TEST(TEST_Cheb, Cheb_first) {
 
-    Solutions<double>(6);
-//работает
+
+    std::vector<double> sol_vec(100);
+    //sol_vec.reserve(100);
+    for (uint32_t i = 0; i < 8; ++i) {
+        sol_vec[i] = chebRootsShake(3)[i];
+        //std::cout << sol_vec[i] << " ";
+    }
+    ASSERT_EQ(sol_vec[0], 0);
+    ASSERT_EQ(sol_vec[1], 7);
+    ASSERT_EQ(sol_vec[2], 3);
+    ASSERT_EQ(sol_vec[3], 4);
+    ASSERT_EQ(sol_vec[4], 1);
+    ASSERT_EQ(sol_vec[5], 6);
+    ASSERT_EQ(sol_vec[6], 2);
+    ASSERT_EQ(sol_vec[7], 5);
 }
