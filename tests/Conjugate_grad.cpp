f922357daa2d87bbe07f2799a17c9c1e3e6cf002
lonertutu <wildfire.aei@gmail.com>
2023-04-08 09:34:58 +0300

    tests

--- tests/Conjugate_grad.cpp
+++ tests/Conjugate_grad.cpp
@@ -39,3 +39,23 @@ TEST(CD, second_test) {
     ASSERT_NEAR(solution[1], 0.497691, 1e-6);
     ASSERT_NEAR(solution[2], 0.0325256, 1e-6);
 }
+
+//Проверка на матрицу большего размера
+TEST(CD, third_test) {
+    std::set<Triplet<double>> data1{{0, 0, 99.}, {0, 1, 12.}, {0, 2, 10.}, {0, 3, 24.},
+                                    {1, 0, 8.}, {1, 1, 100.9}, {1, 2, 0.1}, {1, 3, 0.3},
+                                    {2, 0, 5.}, {2, 1, 4.}, {2, 2, 70.}, {2, 3, 5.},
+                                    {3, 0, 1.}, {3, 1, 3.}, {3, 2, 9.}, {3, 3, 100.}};
+    CSR<double> matrix_iter(4, 4, data1);
+
+    double tolerance = 1e-6;
+    std::vector<double> b = {1., 5., 4., 21.};
+    std::vector<double> init_vec = {0.01, 0.2, 0.5, 0.01};
+
+    auto solution = conjGrad<double>(matrix_iter, b, init_vec, tolerance);
+    std::cout << solution[0] << " " << solution[1] << " " << solution[2] << " " << solution[3];
+    ASSERT_NEAR(solution[3], 0.20504, 1e-6);
+    ASSERT_NEAR(solution[0], -0.050368, 1e-6);
+    ASSERT_NEAR(solution[1], 0.0528952, 1e-6);
+    ASSERT_NEAR(solution[2], 0.0430723, 1e-6);
+}
